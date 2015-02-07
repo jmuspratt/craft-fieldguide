faae61f926492ceb5f65beb78f88f5a598b0ad33
Douglas McDonald <douglas@terrain-design.com>
2015-02-07 02:28:01 -0800

    New feature: "Unused Fields" Section
    _content.html
        - added new "Unused Fields" section
        - tidy tab indentation
    FieldGuideVariables.php
        - added variable method 'getUnusedFieldIds'
        - tidy tab indentation

--- fieldguide/variables/FieldGuideVariable.php
+++ fieldguide/variables/FieldGuideVariable.php
@@ -1,23 +1,62 @@
 <?php
 namespace Craft;
 
-class FieldGuideVariable
-{
-    public function getBlockTypes($matrixFieldId) {
-        $blockTypes = craft()->matrix->getBlockTypesByFieldId($matrixFieldId);
+class FieldGuideVariable {
 
-        $blockTypesList = array();
+	public function getBlockTypes($matrixFieldId) 
+	{
+		$blockTypes = craft()->matrix->getBlockTypesByFieldId($matrixFieldId);
+		
+		$blockTypesList = array();
 
-        foreach($blockTypes as $blockType) {
+		foreach($blockTypes as $blockType) {
 
-            array_push($blockTypesList, array(
-                'name' => $blockType->getAttribute('name'),
-                'handle' => $blockType->getAttribute('handle'),
-                'fields' => $blockType->getFields()
-            ));
+			array_push($blockTypesList, array(
+				'name' => $blockType->getAttribute('name'),
+				'handle' => $blockType->getAttribute('handle'),
+				'fields' => $blockType->getFields()
+			));
+		}
 
-        }
+		return $blockTypesList;
+	}
 
-        return $blockTypesList;
-    }
+	public function getUnusedFieldIds() 
+	{
+		// all field ids
+		$query = craft()->db->createCommand();
+		$allFieldIds = $query
+			->select('craft_fields.id')
+			->from('fields')
+			->order('craft_fields.id')
+			->queryAll();
+		$allFieldIds = self::array_flatten($allFieldIds);
+		
+		// used field ids
+		$query = craft()->db->createCommand();
+		$query->distinct = true;
+		$usedFieldIds = $query
+			->select('craft_fieldlayoutfields.fieldId')
+			->from('fieldlayoutfields')
+			->order('craft_fieldlayoutfields.fieldId')
+			->queryAll();
+		$usedFieldIds = self::array_flatten($usedFieldIds);
+		
+		// unused field ids
+		$unusedFieldIds = array_diff($allFieldIds, $usedFieldIds);
+		
+		return $unusedFieldIds;
+	}
+	
+	private function array_flatten($arr) {
+		$arr = array_values($arr);
+		while (list($k,$v)=each($arr)) {
+			if (is_array($v)) {
+				array_splice($arr,$k,1,$v);
+				next($arr);
+			}
+		}
+		return $arr;
+	}
+
 }
